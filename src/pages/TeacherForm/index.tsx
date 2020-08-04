6d7128ea097a7ebe93f58c2356dd875f99d705d5
Rodrigo Fernandes <rodrigorcfernandes@gmail.com>
2020-08-04 00:02:36 -0300

    first commit

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+const TeacherForm = () => {
+  return (
+    <div className="container" id="page-teacher-form">
+      <PageHeader title="Que incrível que você queira dar aulas." />
+    </div>
+  );
+};
+
+export default TeacherForm;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+const TeacherList = () => {
+  return (
+    <div className="container" id="page-teacher-list">
+      <PageHeader title="Estes são os proffys disponíveis." />
+    </div>
+  );
+};
+
+export default TeacherList;
